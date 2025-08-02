c96f944e9bef9e205e108b47f64679d0dd0f940e
IT24100028 <it24100028@my.sliit.lk>
2025-08-02 17:31:29 +0530

    init the project

---
+++ src/main/java/com/pg30/webechannellingspringboot/WebEchannellingSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.pg30.webechannellingspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEchannellingSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebEchannellingSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pg30/webechannellingspringboot/WebEchannellingSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pg30.webechannellingspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebEchannellingSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
